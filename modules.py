a94f1f6a2ad2ece744215830e32cb01f64ed384c
h21anlus <h21anlus@du.se>
2022-01-24 15:23:38 +0100

    ????

--- modules.py
+++ modules.py
@@ -1,2 +1,21 @@
+
 thousand = list(range(1, 1001))
-print(thousand)
+
+# a = 5
+# b = 2
+
+# result = numbers in thousand / b
+# print(result)
+
+#print(thousand)
+
+    # if quotients == quotients:
+    #     result = int(quotients)
+    # else:
+    #     result = 0
+
+quotients = []
+for number in thousand:
+    quotients.append.int(number // 10)
+
+print(quotients)
