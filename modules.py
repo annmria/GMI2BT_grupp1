087090fd0048098c66147e8ce95f054bca61fbc2
Chelsan98 <96055501+Chelsan98@users.noreply.github.com>
2022-01-25 14:56:25 +0100

    dagens arbete 2022-01-25
    nästan klar med lab 1

--- modules.py
+++ modules.py
@@ -1,19 +1,4 @@
-
 thousand = list(range(1, 1001))
-
-# a = 5
-# b = 2
-
-# result = numbers in thousand / b
-# print(result)
-
-#print(thousand)
-
-    # if quotients == quotients:
-    #     result = int(quotients)
-    # else:
-    #     result = 0
-
 def nummer (x, y):
     quotients = []
     for number in thousand:
@@ -21,3 +6,19 @@ def nummer (x, y):
             quotients.append(number)
     print(quotients)
 nummer (7, 11)
+
+import random 
+number=random.randint (1, 15)
+print ("Välkommen till vårt gissningsspel. Spelet kommer välja ett tal mellan 1-15 och du ska gissa vilket nummer som har valts")
+print ("Gissa ett nummer mellan 1-15")
+guess =input (int ())
+
+if number != guess:
+    
+    print("å nej du suger försök igen")   
+    
+else:
+    print(" tjola hej du hade rätt")
+    
+    
+   
---
+++ program.py
@@ -0,0 +1,3 @@
+import modules 
+
+modules.nummer (8, 11)
